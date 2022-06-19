658f58f4e02b6e776eb61bbbba47311fd63a687e
Mateusz <kroplewskimateusz@gmail.com>
2022-06-19 21:14:06 +0100

    added error toggle to form, need to fix email and img loc

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-let firstname = document.querySelector("#firstname");
-let lastname = document.querySelector("#lastname");
+let firstname = document.querySelector("#firstName");
+let lastname = document.querySelector("#lastName");
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 let button = document.querySelector("#btn");
 
 let firstNameError = document.querySelector("#firstNameError");
 let LastNameError = document.querySelector("#lastNameError");
-let EmailError = document.querySelector("#emailError");
-let PasswordError = document.querySelector("#passwordError");
+let emailError = document.querySelector("#emailError");
+let passwordError = document.querySelector("#passwordError");
 
-function err(borderId, errorDivId) {}
+function showError(borderId, errorDivId) {
+  errorDivId.classList.remove("hidden");
+  borderId.classList.add("border-red-300");
+}
+function removeError(borderId, errorDivId) {
+  errorDivId.classList.add("hidden");
+  borderId.classList.add("border-black");
+}
+
+function isEmpty(inputVar, errorDivId) {
+  if (inputVar.value == "") {
+    showError(inputVar, errorDivId);
+    return false;
+  } else {
+    removeError(inputVar, errorDivId);
+    return true;
+  }
+}
+
+function validateEmail(inputVar, errorDivId) {
+  if (inputVar.value == "" || !inputVar.includes("@")) {
+    showError(inputVar, errorDivId);
+    return false;
+  } else {
+    removeError(inputVar, errorDivId);
+    console.log("true");
+    return true;
+  }
+}
 
 button.addEventListener("click", function (event) {
   event.preventDefault();
-  err();
+
+  //isEmpty(firstname, firstNameError);
+  //isEmpty(lastname, LastNameError);
+  //isEmpty(password, passwordError);
+  validateEmail(email, emailError);
 });
